518a3b2cd338e96e28c5a4fb68059b58d295387c
Luis Antonio Preza <luis.preza@utj.edu.mx>
2021-03-03 19:32:06 -0600

    Recuperacion de contraseña
    Se modifica la cuenta con la que se enviaran los correos a los usuarios

--- Controlador/Sesion/RecupPass.php
+++ Controlador/Sesion/RecupPass.php
@@ -27,18 +27,19 @@ if($result ->num_rows > 0){
     //Instantiation and passing `true` enables exceptions
     $mail = new PHPMailer(true);
     try {
+		$headMail="Content-Type: text/html; charset=UTF-8";
    		//Server settings
-   		$mail->SMTPDebug = 0;//SMTP::DEBUG_SERVER;                      //Enable verbose debug output
+   		$mail->SMTPDebug = 0;                      //Enable verbose debug output
    		$mail->isSMTP();                                            //Send using SMTP
-   		$mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+   		$mail->Host       = 'mail.acumuladoresgarza.com';                     //Set the SMTP server to send through
    		$mail->SMTPAuth   = true;                                   //Enable SMTP authentication
-   		$mail->Username   = 'acumuladores.garza.stw@gmail.com';                     //SMTP username
-   		$mail->Password   = 'GarzAB4t8-/';                               //SMTP password
+   		$mail->Username   = 'no-reply@acumuladoresgarza.com';                     //SMTP username
+   		$mail->Password   = 'jairotieneunahermanayomy';                               //SMTP password
    		$mail->SMTPSecure = PHPMailer::ENCRYPTION_STARTTLS;         //Enable TLS encryption; `PHPMailer::ENCRYPTION_SMTPS` encouraged
    		$mail->Port       = 587;                                    //TCP port to connect to, use 465 for `PHPMailer::ENCRYPTION_SMTPS` above
 
     	//Recipients
-    	$mail->setFrom('acumuladores.garza.stw@gmail.com', 'Garza');
+    	$mail->setFrom('no-reply@acumuladoresgarza.com', 'Acumuladores Garza');
     	$mail->addAddress($correo);     //Add a recipient
     	/*
     	$mail->addAddress('ellen@example.com');               //Name is optional
@@ -52,8 +53,8 @@ if($result ->num_rows > 0){
 	    */
     	//Content
     	$mail->isHTML(true);                                  //Set email format to HTML
-    	$mail->Subject = 'Acceso';
-    	$mail->Body    = 'Ha solicitado la consulta de su clave de acceso;
+    	$mail->Subject = 'Recuperar Contrase&ntilde;a';
+    	$mail->Body    = 'Ha solicitado la consulta de su clave de acceso <br/>
     	                  la cual es la siguiente: '.$pass.' Elimine este correo para mayor seguridad';
     	//$mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
 	
